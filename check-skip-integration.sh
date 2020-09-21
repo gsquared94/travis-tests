cf6341eb0f6a0e9cb38e30b76f272f9855199877
gsquared94 <gaghosh@google.com>
2020-09-21 17:10:11 +0530

    try to fix

--- check-skip-integration.sh
+++ check-skip-integration.sh
@@ -2,8 +2,8 @@
 
 set -e -o pipefail
 
-SKIP=$(git diff --name-only "$TRAVIS_BRANCH"...HEAD | grep -v '^foo/')
-if [ -z "$SKIP" ]; then
-echo Skipping integration tests since only docs changed
-travis_terminate 0
-fi
+git diff --name-only "$TRAVIS_BRANCH"...HEAD | grep -v '^foo/' || {
+    [ -z "$SKIP" ]; then
+    echo Skipping integration tests since only docs changed
+    travis_terminate 0
+}
