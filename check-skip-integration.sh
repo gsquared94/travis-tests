f6ebb5c341448d14238dfc3229686a7cbb2b4154
gsquared94 <gaghosh@google.com>
2020-09-21 16:50:06 +0530

    test removing

--- check-skip-integration.sh
+++ check-skip-integration.sh
@@ -5,5 +5,4 @@ set -e -o pipefail
 reqd=$(git diff --name-only "$TRAVIS_BRANCH"...HEAD | grep -v '^foo/')
 if [ -z "$reqd" ]; then
   echo "Skipping integration tests since only docs changed"
-  travis_terminate 0
 fi
